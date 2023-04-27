5b03a12045aa07aebaf620113e24845d71a99da6
Scyphers <scyphers.dev@gmail.com>
2023-04-27 15:04:46 +1000

    Implement Entity on Player

--- src/entity/player.rs
+++ src/entity/player.rs
@@ -1,6 +1,10 @@
 use crate::inventory::Inventory;
 use crate::stat::{StatSet, StatType};
 
+use super::action::ActionSet;
+use super::Entity;
+
+#[derive(Debug)]
 pub struct Player {
     stats: StatSet,
     health: u64,
@@ -8,34 +12,53 @@ pub struct Player {
 }
 
 impl Player {
-
     pub fn new() -> Player {
         Player {
             stats: StatSet::base_player_stats(),
             health: 100,
-            inventory: Inventory::new(vec![])
+            inventory: Inventory::new(vec![]),
         }
     }
 
-    pub fn damage(self: &mut Self, amount: u64) -> &Player {
-        if self.health < amount {
+    pub fn create(stats: StatSet, health: u64, inventory: Inventory) -> Player {
+        Player {
+            stats,
+            health,
+            inventory,
+        }
+    }
+}
+
+impl Entity for Player {
+    fn get_stat(self: &Self, stat_type: &StatType) -> u64 {
+        self.stats.get_stat(stat_type) + self.inventory.get_stat(stat_type)
+    }
+
+    fn get_max_health(self: &Self) -> u64 {
+        25 + self.get_stat(&StatType::Vitality) * 15
+    }
+
+    fn get_health(self: &Self) -> u64 {
+        self.health
+    }
+
+    fn set_health(self: &mut Self, health: u64) -> &Self {
+        self.health = health;
+        self
+    }
+
+    fn damage(self: &mut Self, amount: u64) -> &Self {
+        if amount >= self.health {
             self.health = 0;
         } else {
-            self.health = self.health - amount;
+            self.health -= amount;
         }
         self
     }
 
-    pub fn calculate_max_health(&self) -> u64 {
-        let vit = self.stats.get_stat(&StatType::Vitality);
-        let vit = vit + self.inventory.get_stat(&StatType::Vitality);
-
-        100 + vit * 15
-    }
-
-    pub fn heal(self: &mut Self, amount: u64) -> &Player {
-        let max = self.calculate_max_health();
-        if self.health + amount >= max {
+    fn heal(self: &mut Self, amount: u64) -> &Self {
+        let max = self.get_max_health();
+        if amount >= max {
             self.health = max;
         } else {
             self.health += amount;
@@ -43,4 +66,7 @@ impl Player {
         self
     }
 
+    fn get_action_set(self: &Self) -> ActionSet {
+        todo!()
+    }
 }
