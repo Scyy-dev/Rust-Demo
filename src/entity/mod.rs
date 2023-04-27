340d9fc69e3ba79b75138fcaeec4e29f167015c1
Scyphers <scyphers.dev@gmail.com>
2023-04-27 15:01:49 +1000

    Add trait for an entity with health and stats

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -1,2 +1,26 @@
-pub mod player;
+use crate::stat::StatType;
+
+use self::action::ActionSet;
+
 pub mod action;
+pub mod player;
+
+pub trait Entity {
+    fn get_stat(self: &Self, stat_type: &StatType) -> u64;
+
+    fn get_max_health(self: &Self) -> u64;
+
+    fn get_health(self: &Self) -> u64;
+
+    fn set_health(self: &mut Self, health: u64) -> &Self;
+
+    fn damage(self: &mut Self, amount: u64) -> &Self;
+
+    fn heal(self: &mut Self, amount: u64) -> &Self;
+
+    fn is_dead(self: &Self) -> bool {
+        self.get_health() == 0
+    }
+
+    fn get_action_set(self: &Self) -> ActionSet;
+}
