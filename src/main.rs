4de2267e59d6184da1f307d31bb23fdca2b55c39
Scyphers <scyphers.dev@gmail.com>
2023-04-27 16:42:14 +1000

    Add simple player damage loop

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@ use rust_demo::{
     entity::{player::Player, Entity},
     inventory::Inventory,
     stat::StatSet,
-    ui::{console, main_menu},
+    ui::main_menu,
 };
 
 fn main() {
     let option = main_menu::main_menu();
-    dbg!(option);
-
     // TODO - interpreting of menu options
-
-    let command = console::read_command("Please enter your first command");
-    dbg!(command);
+    println!("You chose option: {}", option);
+    let mut player = Player::new();
+    for _ in 0..5 {
+        player.damage(5);
+        println!("Player now has {} health", player.get_health())
+    }
 
     let stat_set1 = StatSet::try_from("v:0/a:5/d:0");
     let inventory1 = Inventory::try_from("v:5/a:0/d:0|a\nv:0/a:5/d:0|b\nv:0/a:0/d:5|ab");
