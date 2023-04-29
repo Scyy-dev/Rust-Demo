e4467a92f6dde7f9646f01bcb6ed349763987d43
Scyphers <scyphers.dev@gmail.com>
2023-04-30 01:57:59 +1000

    Add simple loop to battle an enemy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use rust_demo::{
-    entity::{player::Player, Entity},
+    entity::{
+        action::{Action, ActionSet},
+        enemy::SimpleEnemy,
+        player::Player,
+        Actionable,
+    },
     inventory::Inventory,
+    session::Session,
     stat::StatSet,
     ui::main_menu,
 };
@@ -9,14 +15,39 @@ fn main() {
     let option = main_menu::main_menu();
     // TODO - interpreting of menu options
     println!("You chose option: {}", option);
-    let mut player = Player::new();
-    for _ in 0..5 {
-        player.damage(5);
-        println!("Player now has {} health", player.get_health())
-    }
 
-    let stat_set1 = StatSet::try_from("v:0/a:5/d:0");
-    let inventory1 = Inventory::try_from("v:5/a:0/d:0|a\nv:0/a:5/d:0|b\nv:0/a:0/d:5|ab");
-    let player = Player::create(stat_set1.unwrap(), 100, inventory1.unwrap());
-    dbg!(player);
+    // Prepare simple player
+    let player_inv = Inventory::new(vec![]);
+    let player_stats = StatSet::try_from("v:7/a:3/d:3").unwrap();
+    let player = Player::create(player_stats, player_inv);
+
+    // Prepare a simple enemy
+    let enemy_stats = StatSet::try_from("v:0/a:5/d:0");
+    let enemy_actions = ActionSet::try_from("ann");
+    let enemy = SimpleEnemy::new(enemy_stats.unwrap(), enemy_actions.unwrap(), 40);
+
+    let mut session = Session::new(player, enemy);
+
+    for _ in 0..10 {
+        let player_action = Action::Attack;
+        session.player_action(&player_action);
+
+        println!(
+            "Player used {}!\n{}\n{}",
+            player_action,
+            session.player(),
+            session.enemy()
+        );
+
+        let enemy_action = &session.enemy().next_action();
+        session.enemy_action(&enemy_action);
+
+        println!(
+            "Enemy used {}!\n{}\n{}",
+            &enemy_action,
+            session.player(),
+            session.enemy()
+        );
+
+    }
 }
