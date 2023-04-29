0d6b98b3dc94222490eb1a1edc902d783015b4e7
Scyphers <scyphers.dev@gmail.com>
2023-04-30 01:21:27 +1000

    add a session to handle an interaction between player and enemy

---
+++ src/session/mod.rs
@@ -0,0 +1,46 @@
+use crate::{entity::{Entity, action::Action, enemy::SimpleEnemy, player::Player}, stat::StatType};
+
+pub struct Session {
+    player: Player,
+    enemy: SimpleEnemy
+}
+
+impl Session {
+    
+    pub fn new(player: Player, enemy: SimpleEnemy) -> Session {
+        Session { player, enemy }
+    }
+    
+    pub fn player_action(self: &mut Self, player_action: &Action) {
+        resolve(&mut self.player, &mut self.enemy, player_action);
+    }
+    
+    pub fn enemy_action(self: &mut Self, enemy_action: &Action) {
+        resolve(&mut self.enemy, &mut self.player, enemy_action);
+    }
+    
+    pub fn player(self: &Self) -> &Player {
+        &self.player
+    }
+    
+    pub fn enemy(self: &Self) -> &SimpleEnemy {
+        &self.enemy
+    }
+    
+}
+
+fn resolve(entity: &mut dyn Entity, target: &mut dyn Entity, action: &Action) {
+    match action {
+        Action::Attack => {
+            let attack = entity.get_stat(&StatType::Attack);
+            let defence = target.get_stat(&StatType::Defence);
+            if defence >= attack { return; }
+            target.damage(attack - defence);
+        }
+        Action::Heal => {
+            let amount = entity.get_stat(&StatType::Defence);
+            entity.heal(amount);
+        }
+        _ => {}
+    };
+}
