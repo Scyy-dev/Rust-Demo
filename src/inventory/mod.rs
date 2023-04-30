07ae8bcb2220a011ef3139ac7730fb6901d2314c
Scyphers <scyphers.dev@gmail.com>
2023-04-30 23:49:34 +1000

    Added display format for inventories

--- src/inventory/mod.rs
+++ src/inventory/mod.rs
@@ -108,3 +108,13 @@ impl TryFrom<&str> for Inventory {
         }
     }
 }
+
+impl Display for Inventory {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Inventory:\n")?;
+        for item in &self.items {
+            write!(f, "{}", item)?;
+        }
+        Ok(())
+    }
+}
