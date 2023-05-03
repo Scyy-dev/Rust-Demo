271a62f860988b14d9751d06f93d8f6b04055d2b
Scyphers <scyphers.dev@gmail.com>
2023-05-03 21:52:06 +1000

    Overhaul sessions so they support multiple enemies

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -1,14 +1,20 @@
-use std::cell::Cell;
-
 use crate::{
-    entity::{action::Action, enemy::SimpleEnemy, player::Player, Entity},
-    stat::StatType,
+    entity::{
+        action::{Action, ActionSet},
+        enemy::SimpleEnemy,
+        enemy_generator,
+        player::Player,
+        Entity,
+    },
+    inventory::{Inventory, Item},
+    stat::{StatSet, StatType},
 };
 
 pub struct Session {
     player: Player,
     enemy: SimpleEnemy,
-    active: Cell<bool>,
+    difficulty: u64,
+    active: bool,
 }
 
 impl Session {
@@ -16,7 +22,8 @@ impl Session {
         Session {
             player,
             enemy,
-            active: Cell::from(true),
+            difficulty: 1,
+            active: true,
         }
     }
 
@@ -29,7 +36,7 @@ impl Session {
     }
 
     pub fn is_over(&self) -> bool {
-        self.player.is_dead() || self.enemy.is_dead() || !self.active.get()
+        self.player.is_dead() || !self.active
     }
 
     pub fn player(&self) -> &Player {
@@ -40,12 +47,36 @@ impl Session {
         &self.enemy
     }
 
-    pub fn end(&self) {
-        self.active.set(false);
+    pub fn end(&mut self) {
+        self.active = false;
+    }
+
+    pub fn increment_difficulty(&mut self) {
+        self.difficulty = self.difficulty + 1;
+    }
+
+    pub fn next_enemy(&mut self) {
+        self.enemy = enemy_generator::generate_enemy(self.difficulty);
     }
 }
 
-fn resolve(entity: &mut dyn Entity, target: &mut dyn Entity, action: &Action) {
+pub fn demo_session() -> Session {
+    // Prepare simple player
+    let item1 = Item::try_from("Robe of Healing|v:2/a:0/d:3|h").unwrap();
+    let item2 = Item::try_from("Wooden Sword|a:1|a").unwrap();
+    let player_inv = Inventory::new(vec![item1, item2]);
+    let player_stats = StatSet::try_from("v:2/a:3/d:0").unwrap();
+    let player = Player::create(player_stats, player_inv);
+
+    // Prepare a simple enemy
+    let enemy_stats = StatSet::try_from("v:0/a:5/d:0");
+    let enemy_actions = ActionSet::try_from("ann");
+    let enemy = SimpleEnemy::new(enemy_stats.unwrap(), enemy_actions.unwrap(), 40);
+
+    Session::new(player, enemy)
+}
+
+fn resolve(entity: &mut impl Entity, target: &mut impl Entity, action: &Action) {
     match action {
         Action::Attack => {
             let attack = entity.get_stat(&StatType::Attack);
